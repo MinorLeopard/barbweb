46caad83a13086eab66d71a5449cdc8d3c5fdf85
khushxxl <khushaal.choithramani@gmail.com>
2021-12-18 21:29:52 +0530

    don

--- data.js
+++ data.js
@@ -1,30 +1,34 @@
 export const services = [
   {
     id: 1,
-    name: "haircut",
+    name: 'haircut',
     price: 100,
-    duration: "10",
-    desc: "this is a dummy description Accusam diam lorem sadipscing amet amet sanctus rebum et no dolor. Stet dolores diam et aliquyam stet amet at et,.",
+    duration: '10',
+    desc:
+      'this is a dummy description Accusam diam lorem sadipscing amet amet sanctus rebum et no dolor. Stet dolores diam et aliquyam stet amet at et,.',
   },
   {
     id: 2,
-    name: "massage",
+    name: 'massage',
     price: 100,
-    duration: "10",
-    desc: "this is a dummy description Accusam diam lorem sadipscing amet amet sanctus rebum et no dolor. Stet dolores diam et aliquyam stet amet at et,.",
+    duration: '10',
+    desc:
+      'this is a dummy description Accusam diam lorem sadipscing amet amet sanctus rebum et no dolor. Stet dolores diam et aliquyam stet amet at et,.',
   },
   {
     id: 3,
-    name: "styling",
+    name: 'styling',
     price: 100,
-    duration: "10",
-    desc: "this is a dummy description Accusam diam lorem sadipscing amet amet sanctus rebum et no dolor. Stet dolores diam et aliquyam stet amet at et,.",
+    duration: '10',
+    desc:
+      'this is a dummy description Accusam diam lorem sadipscing amet amet sanctus rebum et no dolor. Stet dolores diam et aliquyam stet amet at et,.',
   },
   {
     id: 4,
-    name: "oiling",
+    name: 'oiling',
     price: 100,
-    duration: "10",
-    desc: "this is a dummy description Accusam diam lorem sadipscing amet amet sanctus rebum et no dolor. Stet dolores diam et aliquyam stet amet at et,.",
+    duration: '10',
+    desc:
+      'this is a dummy description Accusam diam lorem sadipscing amet amet sanctus rebum et no dolor. Stet dolores diam et aliquyam stet amet at et,.',
   },
-];
+]
