752104c4c0caad6c3f498d504f1884ff7974b4ac
vansh619 <vanshchaudhary77@gmail.com>
2021-12-20 12:13:31 +0530

    m7

---
+++ .history/components/Hero_20211220121315.tsx
@@ -0,0 +1,87 @@
+import Image from 'next/image'
+import {
+  LocationMarkerIcon,
+  ServerIcon,
+  UserCircleIcon,
+} from '@heroicons/react/solid'
+const Hero = () => {
+  return (
+    <div>
+    <div className="flex flex-col items-center justify-center w-full max-w-full py-5 lg:mb-0 min-w-max">
+      <div className="flex flex-col items-center justify-start w-full max-w-full py-5 lg:mb-0 min-w-max md:space-x-28 md:justify-evenly md:items-start md:flex-row">
+        <div className="flex flex-col items-center justify-center top-10 left-24 md:items-start md:justify-start">
+          <div className="flex items-center justify-center mt-5 rounded-full">
+            <Image
+              src={require('../images/heroi.png')}
+              height={500}
+              width={500}
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center mt-24 ml-20 md:mt-0 md:ml-0">
+          <div className="mt-24 mr-10">
+            <Image
+              src={require('../images/phoonelogo.png')}
+              height={430}
+              width={210}
+            />
+          </div>
+          <div className="mt-0">
+            <Image
+              src={require('../images/mobile1.png')}
+              height={430}
+              width={210}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="cursor-pointer">
+        <Image
+          src={require('../images/playstore.png')}
+          height={100}
+          width={300}
+          className="flex items-center"
+        />
+      </div>
+
+      <div className="flex justify-center items-center max-w-3xl w-full flex-col xl:flex-row h-[10vh] mt-56  xl:mt-10 mb-20 xl:justify-between  xl:ml-0  ">
+        <div className=" max-w-sm  py-10 min-w-max  xl:max-w-4xl flex flex-col xl:flex-row  w-full rounded-3xl bg-white shadow-lg justify-evenly space-y-20 xl:space-y-0 xl:h-[20vh] items-center">
+          <div className="flex items-center justify-center space-x-2 ">
+            <UserCircleIcon className="w-10 h-10" />
+            <div className="text-start">
+              <p className="text-2xl font-bold">0+</p>
+              <p className="font-semibold">Downloads</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center space-x-2 ">
+            <LocationMarkerIcon className="w-10 h-10" />
+            <div className="text-start">
+              <p className="text-2xl font-bold">0+</p>
+              <p className="font-semibold">Locations</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center space-x-2 ">
+            <ServerIcon className="w-10 h-10" />
+            <div className="text-start">
+              <p className="text-2xl font-bold">0+</p>
+              <p className="font-semibold">Reviews</p>
+            </div>
+          </div>
+        </div>
+     
+      </div>
+
+ 
+    </div>
+
+
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#d1d5da" fill-opacity="1" d="M0,32L0,32L288,32L288,160L576,160L576,64L864,64L864,160L1152,160L1152,96L1440,96L1440,0L1152,0L1152,0L864,0L864,0L576,0L576,0L288,0L288,0L0,0L0,0Z"></path>
+      </svg>
+    </div>
+  )
+}
+
+export default Hero
---
+++ .history/components/Navbar_20211220121242.js
@@ -0,0 +1,79 @@
+import { MenuIcon } from '@heroicons/react/outline'
+import { useState } from 'react'
+import Link from 'next/link'
+import Dropdown from './Dropdown.tsx'
+
+const Navbar = () => {
+  const [drop, setDrop] = useState(false)
+  return (
+    <nav className="p-3 sticky bg-white mb-2 z-[99] top-0  flex  max-w-full min-w-max w-full justify-between md:justify-evenly items-center   ">
+      <div>
+        <Link href="/">
+          <p className="text-4xl cursor-pointer">BARB 💇‍♂️</p>
+        </Link>
+      </div>
+      <div>
+        <div className="flex flex-col md:hidden">
+          <MenuIcon
+            onClick={() => {
+              if (drop == true) {
+                setDrop(false)
+              } else {
+                setDrop(true)
+              }
+            }}
+            className="transition-all transform cursor-pointer h-7 w-7 md:hidden"
+          />
+          <div className="">{drop ? <Dropdown /> : null}</div>
+        </div>
+
+        <ul className="hidden space-x-5 md:flex">
+
+          <a href="/ourTeam">
+
+            <li className="transition-all duration-150 transform hover:scale-105 hover:underline">
+              Our team
+            </li>
+          </a>
+          <a href="">
+            <li className="transition-all duration-150 transform hover:scale-105 hover:underline">
+              Cities
+            </li>
+          </a>
+          <a href="">
+            <li className="transition-all duration-150 transform hover:scale-105 hover:underline">
+              Career
+            </li>
+          </a>
+          <a href="">
+            <li className="transition-all duration-150 transform hover:scale-105 hover:underline">
+              Press
+            </li>
+          </a>
+          <a href="">
+            <li className="transition-all duration-150 transform hover:scale-105 hover:underline">
+              Report
+            </li>
+          </a>
+
+          <a href="/contactUs">
+            <li className="transition-all duration-150 transform hover:scale-105 hover:underline">
+              Contact us
+            </li>
+          </a>
+        </ul>
+      </div>
+
+      <div>
+        <a href="/login" className="">
+
+        <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+  ADMIN
+</button>
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
